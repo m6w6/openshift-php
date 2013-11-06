5fcf27594a23d9403f993f26a8b2d5654740b3ca
Michael Wallner <mike@php.net>
2013-11-06 16:19:21 +0100

    flush

--- .openshift/deploy.mk
+++ .openshift/deploy.mk
@@ -1,4 +1,5 @@
 PECLCMD = ./php/bin/pecl -c .pearrc
+PEARCMD = ./php/bin/pear -c .pearrc
 PHPEXTS = $(shell ./php/bin/php-config --extension-dir)
 CURRENT = $(OPENSHIFT_DEPLOYMENTS_DIR)/current/repo/
 
@@ -7,13 +8,16 @@ all: ini httpd/logs/httpd.pid
 
 ini:
 	$(PECLCMD) config-set php_ini $(OPENSHIFT_DATA_DIR)/php/lib/php.ini
+	$(PEARCMD) config-set php_ini $(OPENSHIFT_DATA_DIR)/php/lib/php.ini
 
-$(PHPEXTS)/raphf.so: httpd/modules/libphp5.so
+$(PHPEXTS)/raphf.so: | httpd/modules/libphp5.so
 	$(PECLCMD) install -f raphf
-$(PHPEXTS)/propro.so: httpd/modules/libphp5.so
+$(PHPEXTS)/propro.so: | httpd/modules/libphp5.so
 	$(PECLCMD) install -f propro
 $(PHPEXTS)/http.so: $(PHPEXTS)/raphf.so $(PHPEXTS)/propro.so
 	$(PECLCMD) install -f pecl_http
+$(PHPEXTS)/discount.so: | httpd/modules/libphp5.so
+	$(PECLCMD) install -f markdown
 php/lib/php.ini: $(CURRENT)/.openshift/php.ini.in
 	(echo "; generated file; do not edit"; envsubst < $^) > $@
 httpd/conf/httpd.conf: $(CURRENT)/.openshift/httpd.conf.in
