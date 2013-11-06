99a27d744b20f9c49d0563ce3b5ac520f06f99db
Michael Wallner <mike@php.net>
2013-11-06 22:52:43 +0100

    attempt to fix pecl exts

--- .openshift/deploy.mk
+++ .openshift/deploy.mk
@@ -1,28 +1,34 @@
-PECLCMD = ./php/bin/pecl -c .pearrc
-PEARCMD = ./php/bin/pear -c .pearrc
-PHPEXTS = $(shell ./php/bin/php-config --extension-dir)
-CURRENT = $(OPENSHIFT_DEPLOYMENTS_DIR)/current/repo/
+PHPINI   = ./php/lib/php.ini
+PECLCMD  = ./php/bin/pecl -c .pearrc
+PEARCMD  = ./php/bin/pear -c .pearrc
+CURRENT  = $(OPENSHIFT_DEPLOYMENTS_DIR)/current/repo/
+EXTDIR   = $(shell ./php/bin/php-config --extension-dir)
+EXTNAMES = $(shell ./dep.awk < pecl.dep | cut -f1)
+VERSIONS = $(shell ./dep.awk < pecl.dep | cut -f2)
+LIBNAMES = $(shell ./dep.awk < pecl.dep | cut -f3)
+EXTFILES = $(addprefix $(EXTDIR)/, $(LIBNAMES))
 
-all: ini $(CURRENT)/public httpd/logs/httpd.pid
+all: ini $(EXTFILES) httpd/conf/httpd.conf
+	for ext in $(LIBNAMES); do grep -Eq "^[[:space:]]*extension[[:space::]*=[[:space:]]*$$ext" $(PHPINI) || echo "extension=$$ext" >> $(PHPINI); done
+
 .PHONY: ini all
 
 $(CURRENT)/public:
 	mkdir -p $@
-ini:
-	$(PECLCMD) config-set php_ini $(OPENSHIFT_DATA_DIR)/php/lib/php.ini
-	$(PEARCMD) config-set php_ini $(OPENSHIFT_DATA_DIR)/php/lib/php.ini
 
-$(PHPEXTS)/raphf.so: | httpd/modules/libphp5.so
-	$(PECLCMD) install -f raphf
-$(PHPEXTS)/propro.so: | httpd/modules/libphp5.so
-	$(PECLCMD) install -f propro
-$(PHPEXTS)/http.so: $(PHPEXTS)/raphf.so $(PHPEXTS)/propro.so
-	$(PECLCMD) install -f pecl_http
-$(PHPEXTS)/discount.so: | httpd/modules/libphp5.so
-	$(PECLCMD) install -f markdown
-php/lib/php.ini: $(CURRENT)/.openshift/php.ini.in
+ini: $(PHPINI)
+	$(PECLCMD) config-set php_ini $(OPENSHIFT_DATA_DIR)/$(PHPINI)
+	$(PEARCMD) config-set php_ini $(OPENSHIFT_DATA_DIR)/$(PHPINI)
+
+$(EXTDIR)/%: | httpd/modules/libphp5.so
+	aE=($(EXTNAMES)); aV=($(VERSIONS)); aL=($(LIBNAMES)); \
+	for ((i=0; i < $${#aE[@]}; i++)); do \
+		if test $* = $${aL[$$i]}; then \
+			$(PECLCMD) install -f $${aE[$$i]}-$${aV[$$i]}; \
+		fi; \
+	done
+
+$(PHPINI): $(CURRENT)/.openshift/php.ini.in
 	(echo "; generated file; do not edit"; envsubst < $^) > $@
-httpd/conf/httpd.conf: $(CURRENT)/.openshift/httpd.conf.in
+httpd/conf/httpd.conf: $(CURRENT)/.openshift/httpd.conf.in | $(CURRENT)/public
 	(echo "# generated file; do not edit"; envsubst < $^) > $@
-httpd/logs/httpd.pid: httpd/conf/httpd.conf php/lib/php.ini
-	test -f $@ && ./httpd/bin/apachectl restart || true
