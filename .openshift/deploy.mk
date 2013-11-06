eeaf661cc4267f975de38eaa5709e5a37ef34a76
Michael Wallner <mike@php.net>
2013-11-06 22:56:35 +0100

    use $CURRENT

--- .openshift/deploy.mk
+++ .openshift/deploy.mk
@@ -3,9 +3,9 @@ PECLCMD  = ./php/bin/pecl -c .pearrc
 PEARCMD  = ./php/bin/pear -c .pearrc
 CURRENT  = $(OPENSHIFT_DEPLOYMENTS_DIR)/current/repo/
 EXTDIR   = $(shell ./php/bin/php-config --extension-dir)
-EXTNAMES = $(shell ./dep.awk < pecl.dep | cut -f1)
-VERSIONS = $(shell ./dep.awk < pecl.dep | cut -f2)
-LIBNAMES = $(shell ./dep.awk < pecl.dep | cut -f3)
+EXTNAMES = $(shell $(CURRENT)/dep.awk < $(CURRENT)/pecl.dep | cut -f1)
+VERSIONS = $(shell $(CURRENT)/dep.awk < $(CURRENT)/pecl.dep | cut -f2)
+LIBNAMES = $(shell $(CURRENT)/dep.awk < $(CURRENT)/pecl.dep | cut -f3)
 EXTFILES = $(addprefix $(EXTDIR)/, $(LIBNAMES))
 
 all: ini $(EXTFILES) httpd/conf/httpd.conf
