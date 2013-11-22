86eea24d60ddd8b839e66d80846f01cc918c6c13
Michael Wallner <mike@php.net>
2013-11-22 14:54:29 +0100

    apu depends on apr of course

--- .openshift/build.mk
+++ .openshift/build.mk
@@ -17,6 +17,7 @@ pcre-$(PCRE_VERSION)/.libs/libpcre.so: pcre-$(PCRE_VERSION)/Makefile
 	cd pcre-$(PCRE_VERSION) && make -s -j3 V=0 || make
 pcre/lib/libpcre.so: pcre-$(PCRE_VERSION)/.libs/libpcre.so
 	cd pcre-$(PCRE_VERSION) && make -s install V=0
+
 apr-$(APR_VERSION)/configure:
 	curl http://tweedo.com/mirror/apache//apr/apr-$(APR_VERSION).tar.bz2 | tar xj
 apr-$(APR_VERSION)/config.status: apr-$(APR_VERSION)/configure
@@ -27,26 +28,29 @@ apr-$(APR_VERSION)/.libs/libapr-1.so: apr-$(APR_VERSION)/Makefile
 	cd apr-$(APR_VERSION) && make -s -j3 V=0 || make
 apr/lib/libapr-1.so: apr-$(APR_VERSION)/.libs/libapr-1.so
 	cd apr-$(APR_VERSION) && make -s install V=0
+
 apr-util-$(APU_VERSION)/configure:
 	curl http://tweedo.com/mirror/apache//apr/apr-util-$(APU_VERSION).tar.bz2 | tar xj
-apr-util-$(APU_VERSION)/config.status: apr-util-$(APU_VERSION)/configure
+apr-util-$(APU_VERSION)/config.status: apr-util-$(APU_VERSION)/configure 
 	cd apr-util-$(APU_VERSION) && ./configure -C --prefix=$(OPENSHIFT_DATA_DIR)/apu --with-apr=$(OPENSHIFT_DATA_DIR)/apr
-apr-util-$(APU_VERSION)/Makefile: apr-util-$(APU_VERSION)/config.status
+apr-util-$(APU_VERSION)/Makefile: apr-util-$(APU_VERSION)/config.status apr/lib/libapr-1.so
 	cd apr-util-$(APU_VERSION) && ./config.status -q
 apr-util-$(APU_VERSION)/.libs/libaprutil-1.so: apr-util-$(APU_VERSION)/Makefile
 	cd apr-util-$(APU_VERSION) && make -s -j3 V=0 || make
 apu/lib/libaprutil-1.so: apr-util-$(APU_VERSION)/.libs/libaprutil-1.so
 	cd apr-util-$(APU_VERSION) && make -s install V=0
+
 httpd-$(APACHE_VERSION)/configure:
 	curl http://tweedo.com/mirror/apache//httpd/httpd-$(APACHE_VERSION).tar.bz2 | tar xj
 httpd-$(APACHE_VERSION)/config.status: pcre/lib/libpcre.so apu/lib/libaprutil-1.so apr/lib/libapr-1.so httpd-$(APACHE_VERSION)/configure
 	cd httpd-$(APACHE_VERSION) &&  ./configure -C --prefix=$(OPENSHIFT_DATA_DIR)/httpd --enable-so --enable-http --enable-rewrite --with-mpm=prefork --with-apr=$(OPENSHIFT_DATA_DIR)/apr --with-apr-util=$(OPENSHIFT_DATA_DIR)/apu --with-pcre=$(OPENSHIFT_DATA_DIR)/pcre
-httpd-$(APACHE_VERSION)/Makefile: httpd-$(APACHE_VERSION)/config.status -q
-	cd httpd-$(APACHE_VERSION) && ./config.status
+httpd-$(APACHE_VERSION)/Makefile: httpd-$(APACHE_VERSION)/config.status
+	cd httpd-$(APACHE_VERSION) && ./config.status -q
 httpd-$(APACHE_VERSION)/support/apxs: httpd-$(APACHE_VERSION)/Makefile
 	cd httpd-$(APACHE_VERSION) && make -s -j3 V=0 || make
 httpd/bin/apxs:  httpd-$(APACHE_VERSION)/support/apxs
 	cd httpd-$(APACHE_VERSION) && make -s install V=0
+
 php-$(PHP_VERSION)/configure:
 	curl http://us1.php.net/distributions/php-$(PHP_VERSION).tar.bz2 | tar xj
 php-$(PHP_VERSION)/config.status: httpd/bin/apxs php-$(PHP_VERSION)/configure
