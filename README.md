1acd944115310d8d1343e68b267fd103c123d3d8
Michael Wallner <mike@php.net>
2013-12-18 14:44:32 +0100

    backport

--- README.md
+++ README.md
@@ -4,18 +4,28 @@ Build and setup Apache-2.4 and PHP-5.5 on RedHat's Openshift PaaS
 
 ## How to start:
 
-### As merged subtree
-
-```
-$ git remote add openshift-php https://github.com/m6w6/openshift-php.git
-$ git fetch openshift-php
-$ git read-tree --prefix=.openshift/ -u openshift-php/master
-```
-
 ### As submodule
 
 ```
 $ git submodule add git://github.com/m6w6/openshift-php.git .openshift
+```
+
+### As merged subtree
+
+```
+$ git remote add openshift-php https://github.com/m6w6/openshift-php.git
+$ git checkout -b openshift-php openshift-php/master
+$ git checkout master
+$ git read-tree --prefix=.openshift/ -u openshift-php
+```
+
+#### Updating a merged subtree
+
+```
+$ git checkout openshift-php
+$ git pull
+$ git checkout master
+$ git merge -s recursive -X subtree=.openshift openshift-php
 ```
 
 
--- deploy.mk
+++ deploy.mk
@@ -7,11 +7,17 @@ EXTNAMES = $(shell $(CURRENT)/.openshift/dep.awk < $(CURRENT)/.openshift/pecl.de
 VERSIONS = $(shell $(CURRENT)/.openshift/dep.awk < $(CURRENT)/.openshift/pecl.dep | cut -f2)
 LIBNAMES = $(shell $(CURRENT)/.openshift/dep.awk < $(CURRENT)/.openshift/pecl.dep | cut -f3)
 EXTFILES = $(addprefix $(EXTDIR)/, $(LIBNAMES))
+PROJECTS = $(wildcard $(CURRENT)/*/openshift.mk)
 
-all: ini $(EXTFILES) httpd/conf/httpd.conf
+export
+
+all: ini $(EXTFILES) httpd/conf/httpd.conf mk-projects
 	for ext in $(LIBNAMES); do grep -Eq "^[[:space:]]*extension[[:space:]]*=[[:space:]]*$$ext" $(PHPINI) || echo "extension=$$ext" >> $(PHPINI); done
 
-.PHONY: ini all
+.PHONY: ini all mk-projects
+
+mk-projects:
+	$(foreach project, $(PROJECTS), cd $(dir $(project)) && $(MAKE) -f openshift.mk)
 
 $(CURRENT)/public:
 	mkdir -p $@
@@ -20,7 +26,7 @@ ini: $(PHPINI)
 	$(PECLCMD) config-set php_ini $(OPENSHIFT_DATA_DIR)/$(PHPINI)
 	$(PEARCMD) config-set php_ini $(OPENSHIFT_DATA_DIR)/$(PHPINI)
 
-$(EXTDIR)/%: | httpd/modules/libphp5.so
+$(EXTDIR)/%: | httpd/modules/libphp5.so $(CURRENT)/.openshift/pecl.dep
 	aE=($(EXTNAMES)); aV=($(VERSIONS)); aL=($(LIBNAMES)); \
 	for ((i=0; i < $${#aE[@]}; i++)); do \
 		if test $* = $${aL[$$i]}; then \
